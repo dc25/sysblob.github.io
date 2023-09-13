8efee4a19996e69ea514ff4e3eb4f370c5a4bb67
dave <dave>
2023-09-13 11:59:33 -0700

    gh-pages commit

---
+++ assets/js/data/swcache.js
@@ -0,0 +1,58 @@
+const resource = [
+  /* --- CSS --- */
+  '/sysblob.github.io/assets/css/jekyll-theme-chirpy.css',
+
+  /* --- PWA --- */
+  '/sysblob.github.io/app.js',
+  '/sysblob.github.io/sw.js',
+
+  /* --- HTML --- */
+  '/sysblob.github.io/index.html',
+  '/sysblob.github.io/404.html',
+
+  
+    '/sysblob.github.io/categories/',
+  
+    '/sysblob.github.io/tags/',
+  
+    '/sysblob.github.io/archives/',
+  
+    '/sysblob.github.io/about/',
+  
+
+  /* --- Favicons & compressed JS --- */
+  
+  
+    '/sysblob.github.io/assets/img/favicons/android-chrome-192x192.png',
+    '/sysblob.github.io/assets/img/favicons/android-chrome-512x512.png',
+    '/sysblob.github.io/assets/img/favicons/apple-touch-icon.png',
+    '/sysblob.github.io/assets/img/favicons/favicon-16x16.png',
+    '/sysblob.github.io/assets/img/favicons/favicon-32x32.png',
+    '/sysblob.github.io/assets/img/favicons/favicon.ico',
+    '/sysblob.github.io/assets/img/favicons/mstile-150x150.png',
+    '/sysblob.github.io/assets/img/favicons/safari-pinned-tab.svg',
+    '/sysblob.github.io/assets/js/dist/categories.min.js',
+    '/sysblob.github.io/assets/js/dist/commons.min.js',
+    '/sysblob.github.io/assets/js/dist/home.min.js',
+    '/sysblob.github.io/assets/js/dist/misc.min.js',
+    '/sysblob.github.io/assets/js/dist/page.min.js',
+    '/sysblob.github.io/assets/js/dist/post.min.js'
+];
+
+/* The request url with below domain will be cached */
+const allowedDomains = [
+  
+
+  'sysblob.com',
+
+  
+
+  'fonts.gstatic.com',
+  'fonts.googleapis.com',
+  'cdn.jsdelivr.net',
+  'polyfill.io'
+];
+
+/* Requests that include the following path will be banned */
+const denyUrls = [];
+
